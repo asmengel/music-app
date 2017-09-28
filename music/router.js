79fa49b470b224141eeb414b034d1c0d3858497e
Anthony Mengel <anthonymengel1232@gmail.com>
2017-09-28 10:00:12 -0400

    working on vote endpoint

--- music/router.js
+++ music/router.js
@@ -318,7 +318,18 @@ router.delete('/playlist/:id', jwtAuth, (req, res) => {
 // end router.delete (delete a playlist)
 
 // vote on a song
-router.put('/vote/:id', jwtAuth, (req, res) => {
+router.put('/vote/:id', (req, res) => {
+  Artist
+  findByIdAndUpdate(req.body.album.$.votes)
+  .then(() => {
+    res.status(204).json({ message: 'you upvoted'});
+  })
+  .catch(err => {
+    console.error(err);
+    res.status(500).json({error: 'wrong on vote update'});
+  });
+  // find content to vote on
+  // 
 
 // check for required query parameters
 }); // end router.put (vot on a song)
